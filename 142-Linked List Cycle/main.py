5027b3be67e2dd2889f165f7176379c1f2d689c3
Tom Arad <tom.arad@wsu.edu>
2023-03-09 09:22:00 -0800

    Linked List Cycle

---
+++ 142-Linked List Cycle/main.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution:
+    def detectCycle(self, head):
+        node_set = set()
+        while(head != None):
+            if(head in node_set):
+                return head
+            node_set.add(head)
+            head = head.next
